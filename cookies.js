922e9d4aedd2a866dd56d156943a6a83de4cf4ce
Will Farhat <willfarhat@pop-os.localdomain>
2021-05-28 19:54:28 -0700

    cross-storage

--- cookies.js
+++ cookies.js
@@ -1,15 +1,19 @@
 function isEmpty(obj) {
-    return (Object.keys(obj).length === 0);
+  return Object.keys(obj).length === 0;
 }
 
 function checkCookieExists() {
-    const username = localStorage.getItem("username");
-    if (username == null) {
-        window.location.href = "./login/index.html";
-    }
+    const storage = new CrossStorageClient("http://localhost:3000/hub.html");
+    storage.onConnect().then(function() {
+        username=storage.get("username");
+        console.log(username)
+        if (username==null){
+            window.location.href = "./login/index.html";
+        }
+    });
 }
 
 function setName(name) {
-    localStorage.setItem("username", name)
-    window.location.href = "../index.html";
+  localStorage.setItem("username", name);
+  //window.location.href = "../index.html";
 }

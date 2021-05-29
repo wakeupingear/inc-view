d97de9cb60797021f6a26c31781f6a11cb673d24
Will Farhat <35930179+willf668@users.noreply.github.com>
2021-05-29 14:30:19 -0700

    added node

--- node/node.js
+++ node/node.js
@@ -1,1 +1,12 @@
 const WebSocket = require('ws');
+require("../enumsModule.js");
+const ws = new WebSocket('ws://localhost:8000');
+const config = require('./config.json'); 
+
+ws.onopen=function(){
+    console.log("Connected to server");
+    let _data=config;
+    _data.header=packetType.serverConnect;
+    _data=JSON.stringify(_data);
+    ws.send(_data);
+}

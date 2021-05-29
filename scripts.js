2931c5da7e77fadde8ed8c53e2f3508f8c0c7641
Will Farhat <35930179+willf668@users.noreply.github.com>
2021-05-28 23:53:20 -0700

    coach/participant views

--- scripts.js
+++ scripts.js
@@ -4,13 +4,22 @@ function checkCookieExists(){
         fetch("http://willfarhat.com/incView/checkName.php?username="+username)
         .then(function(req){
             req.text().then(function(text){
-                console.log(text)
+                if (text=="coach"||true){
+                    console.log("Coach");
+                    document.getElementById("content").innerHTML="<object type='text/html' data='coach.html'></object>";
+                }
+                else if (text=="participant"){
+                    console.log("Participant");
+                    document.getElementById("content").innerHTML="<object type='text/html' data='participant.html'></object>";
+                }
+                else console.log("Access denied");
             })
         })
     }
 }
 
 function setName(username){
+    username=username.toLowerCase().replace(/\s/g, '');
     localStorage.setItem("username",username);
     checkCookieExists();
 }

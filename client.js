fbc7f4910364eac68058a930a35eca91149f7b09
Will Farhat <35930179+willf668@users.noreply.github.com>
2021-05-29 23:55:37 -0700

    sidebar

--- client.js
+++ client.js
@@ -1,30 +1,57 @@
-const ws = new WebSocket('ws://localhost:8000');
+function connect() {
+    const ws = new WebSocket('ws://localhost:8000');
 
-ws.onopen = function () {
-    console.log("Connected to server");
-    let _data = JSON.stringify({
-        header: packetType.clientConnect
-    });
-    ws.send(_data);
-}
-ws.onmessage = function (event) {
-    _data = JSON.parse(event.data); //Parse data as a JS object
-    switch (_data.header) {
-        case packetType.clientStartViewing:
-            if (_data.location != "") {
-                console.log("New room: " + _data.location);
-
-                const domain = 'meet.jit.si';
-                const options = {
-                    roomName: _data.location,
-                    parentNode: document.querySelector("#video")
-                };
-                const api = new JitsiMeetExternalAPI(domain, options);
-            }
-            else {
-                document.querySelector("#video").innerHTML="";
-            }
-            break;
-        default: break;
+    ws.onopen = function () {
+        console.log("Connected to server");
+        let _data = JSON.stringify({
+            header: packetType.clientConnect
+        });
+        ws.send(_data);
     }
-};
+    ws.onmessage = function (event) {
+        _data = JSON.parse(event.data); //Parse data as a JS object
+        switch (_data.header) {
+            case packetType.clientStartViewing:
+                if (_data.location != "") {
+                    console.log("New room: " + _data.location);
+
+                    const domain = 'meet.jit.si';
+                    const options = {
+                        roomName: _data.location,
+                        parentNode: document.querySelector("#video")
+                    };
+                    const api = new JitsiMeetExternalAPI(domain, options);
+                }
+                else {
+                    document.querySelector("#video").innerHTML = "";
+                }
+                break;
+            default: break;
+        }
+    };
+
+    let retry = function (e) {
+        setTimeout(function () {
+            console.log("Disconnected. Retrying in 3s");
+            connect();
+        }, 3000);
+    }
+    ws.onerror = retry;
+    ws.onclose = retry;
+}
+connect();
+
+let sidebarOpened=true;
+function setSidebarWidth() {
+    const bar=document.querySelector("#sidebar");
+    if (sidebarOpened){
+        bar.classList.remove("opening");
+        bar.classList.add("closing");
+        sidebarOpened=false;
+    }
+    else {
+        bar.classList.remove("closing");
+        bar.classList.add("opening");
+        sidebarOpened=true;
+    }
+}
--- node/node.js
+++ node/node.js
@@ -1,12 +1,25 @@
-const WebSocket = require('ws');
 require("../enumsModule.js");
-const ws = new WebSocket('ws://localhost:8000');
-const config = require('./config.json'); 
 
-ws.onopen=function(){
-    console.log("Connected to server");
-    let _data=config;
-    _data.header=packetType.serverConnect;
-    _data=JSON.stringify(_data);
-    ws.send(_data);
+function connect() {
+    const WebSocket = require('ws');
+    const ws = new WebSocket('ws://localhost:8000');
+    const config = require('./config.json');
+
+    ws.onopen = function () {
+        console.log("Connected to server");
+        let _data = config;
+        _data.header = packetType.serverConnect;
+        _data = JSON.stringify(_data);
+        ws.send(_data);
+    }
+
+    let retry = function (e) {
+        setTimeout(function(){
+            connect();
+        },3000);
+    }
+    ws.onerror=retry;
+    ws.onclose=retry;
 }
+
+connect();

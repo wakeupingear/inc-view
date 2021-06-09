bcef637b6176da6ef6c0c870d89b6fd1fbc2c285
Will Farhat <35930179+willf668@users.noreply.github.com>
2021-06-08 22:32:01 -0700

    url change

--- scripts.js
+++ scripts.js
@@ -3,8 +3,8 @@ function checkCookieExists() {
     const setButton = document.getElementById("setStorage");
     setButton.style.display = "none";
     if (myUsername !== "" && myUsername !== null) {
-        fetch("http://localhost:8080?username=" + myUsername)
-        //fetch("https://52.35.162.61:8080?username=" + myUsername)
+        //fetch("http://localhost:8080?username=" + myUsername)
+        fetch("http://52.35.162.61:8080?username=" + myUsername)
             .then(function (req) {
                 req.text().then(function (text) {
                     console.log(text)
